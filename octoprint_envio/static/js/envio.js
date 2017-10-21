f6b707dfb8b7e4d6d5d85793664f6ad91d363212
Styku <brtstyczen@gmail.com>
2017-10-21 21:02:03 +0000

    Adding sensors from settings panel

--- octoprint_envio/static/js/envio.js
+++ octoprint_envio/static/js/envio.js
@@ -5,7 +5,9 @@ $(function() {
         self.global_settings = parameters[1];
         self.sensor_settings = ko.observableArray();
         self.devices = ko.observableArray();
-
+        self.devTypesId = [0, 1];
+        self.devTypesLabels = ['Discrete', 'W1'];
+        self.devTypes = [{'name':'Discrete', 'id':0},{'name':'W1', 'id':1}];
         self.onBeforeBinding = function(){
             self.sensor_settings(self.global_settings.settings.plugins.envio.sensors())
         }
@@ -15,9 +17,8 @@ $(function() {
         };
 
         self.addDevice = function(){
-            self.sensor_settings.push({'name':'New sensor','GPIO':2,'path':'/sys/bus/w1/devices/'});
+            self.sensor_settings.push({'name':'New sensor','gpio':2,'path':'/sys/bus/w1/devices/', 'direction':0, 'type':0});
             alert(ko.toJSON(self.sensor_settings));
-            alert(ko.toJSON(self.devices));
         };
 
         self.removeDevice = function(){
